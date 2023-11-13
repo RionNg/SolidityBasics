f5aa7d68e48bfbd57378d7e1ecb5086612b19c2a
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-13 22:25:02 +0800

    Add files via upload

---
+++ Solidity Basics/mapping.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+contract MyContract {
+    uint256 public peopleCount = 0;
+    mapping(uint256 => Person) public people;
+    // mapping(key => value)
+    
+    struct Person {
+        uint256 _id;
+        string _firstName;
+        string _lastName;
+    }
+
+    function addPerson(
+        string memory _firstName,
+        string memory _lastName
+        ) public {
+            peopleCount += 1;
+            people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+
+}
