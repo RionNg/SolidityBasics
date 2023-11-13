defa90c954f32bfd2338d35708d2a08ba510d4d3
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-13 22:06:06 +0800

    Add files via upload

---
+++ Solidity Basics/struct.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+contract MyContract {
+    Person[] public people;
+
+    uint256 public peopleCount;
+    
+    struct Person {
+        string _firstName;
+        string _lastName;
+    }
+
+    function addPerson(
+        string memory _firstName,
+        string memory _lastName
+        ) public {
+            people.push(Person(_firstName, _lastName));
+            peopleCount += 1;
+    }
+
+}
