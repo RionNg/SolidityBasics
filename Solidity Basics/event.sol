b254d99d3c6b717aedf78f3f9ca698908e0e7290
RionNG <147371006+RionNg@users.noreply.github.com>
2023-11-14 16:04:25 +0800

    Add files via upload

---
+++ Solidity Basics/event.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+contract MyContract {
+
+    mapping(address => uint256) public balances;
+    address payable wallet;
+
+    event Purchase(
+        address indexed _buyer,
+        uint256 _amount
+    );
+
+    constructor(address payable _wallet) {
+        wallet = _wallet;
+    }
+
+    function buyToken() public payable {
+        //buy a token
+        balances[msg.sender] += 1;
+        //send ether to the wallet
+        wallet.transfer(msg.value);
+        emit Purchase(msg.sender, 1);
+    }
+
+}
